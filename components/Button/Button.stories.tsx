74a78af65c3ed6ec5550d841d3a96519f6c9d025
Mahya Cheraghi <mahya_ch@yahoo.com>
2025-03-12 15:27:46 +0330

    feat(button): add all button styles

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,31 +1,65 @@
-import type { Meta, StoryObj } from "@storybook/react"
-import { Button } from "./Button"
+// import type { Meta, StoryObj } from "@storybook/react"
+// import { Button } from "./Button"
+
+// const meta: Meta<typeof Button> = {
+//   title: "Button",
+//   component: Button,
+//   args: {
+//     intent: "primary",
+//     underline: false,
+//     children: "Button",
+//     size: "lg",
+//   },
+//   argTypes: {
+//     intent: {
+//       options: ["primary", "secondary"],
+//       control: { type: "select" },
+//     },
+//     size: {
+//       options: ["sm", "lg"],
+//       control: { type: "select" },
+//     },
+//   },
+// }
+
+// type Story = StoryObj<typeof Button>
+
+// export const Default: Story = {
+//   render: (args) => <Button {...args} />,
+// }
+
+// export default meta
+
+
+// button.stories.tsx
+
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
   title: "Button",
   component: Button,
   args: {
     intent: "primary",
-    underline: false,
     children: "Button",
-    size: "lg",
+    size: "medium",
   },
   argTypes: {
     intent: {
-      options: ["primary", "secondary"],
+      options: ["primary", "secondary", "outline", "text"],
       control: { type: "select" },
     },
     size: {
-      options: ["sm", "lg"],
+      options: ["large", "medium", "small"],
       control: { type: "select" },
     },
   },
-}
+};
 
-type Story = StoryObj<typeof Button>
+type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   render: (args) => <Button {...args} />,
-}
+};
 
-export default meta
+export default meta;
