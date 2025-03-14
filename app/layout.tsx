f3e8a91a059c35faa686e2e6537100c0c6ff0650
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2025-03-14 03:46:53 +0330

    fix(layout): Change HTML language to Persian and set text direction to RTL

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "styles/tailwind.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl" className="font-family-anjoman-max">
       <body>{children}</body>
     </html>
   )
