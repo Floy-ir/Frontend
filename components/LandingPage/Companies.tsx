b637416542c4209c7eae903aa6ea37933a420d3f
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2025-05-18 19:10:49 +0330

    refactor(Companies): update image styling and dimensions for better layout consistency

--- components/LandingPage/Companies.tsx
+++ components/LandingPage/Companies.tsx
@@ -6,23 +6,23 @@ export default function Companies() {
         <div className="flex items-center justify-start gap-6 lg:justify-center">
           <div className="mx-5 flex flex-row items-center justify-center gap-10 md:mx-0 md:gap-20 lg:gap-33">
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="" src="/images/alibaba.png" alt="Alibaba" width={100} height={10} />
+            <Image className="h-9 object-contain" src="/images/alibaba.png" alt="Alibaba" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="" src="/images/MrBilit.png" alt="safarmarket" width={100} height={10} />
+            <Image className="h-9 object-contain" src="/images/MrBilit.png" alt="safarmarket" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="" src="/images/flightio.png" alt="Flightio" width={100} height={100} />
+            <Image className="h-9 object-contain" src="/images/flightio.png" alt="Flightio" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="" src="/images/flytoday.png" alt="Felanito" width={100} height={33} />
+            <Image className="h-9 object-contain" src="/images/flytoday.png" alt="Felanito" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="" src="/images/snapptrip.png" alt="SnappTrip" width={60} height={10} />
+            <Image className="h-9 object-contain" src="/images/snapptrip.png" alt="SnappTrip" width={100} height={36} />
             {/* </div> */}
           </div>
           {/*  todo : add other websites */}
