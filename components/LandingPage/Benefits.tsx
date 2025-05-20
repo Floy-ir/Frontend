27b222adb04f37e02c339e14ff68b54c823bc8ca
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2025-05-20 10:06:56 +0330

    refactor(Benefits, Companies): improve layout and responsiveness, including adjustments to spacing, image sizes, and flex properties for better mobile and desktop views

--- components/LandingPage/Benefits.tsx
+++ components/LandingPage/Benefits.tsx
@@ -9,8 +9,8 @@ import line from "../../public/images/Underline_06_small.svg"
 
 export default function Benefits() {
   return (
-    <div className="w-full justify-start overflow-x-auto md:flex md:items-center md:justify-center">
-      <div className="mb-4 mt-8 inline-flex h-[54px] min-w-max shrink-0 snap-x snap-mandatory items-center justify-center gap-18">
+    <div className="w-full pb-2 md:pb-0 overflow-x-auto md:flex md:items-center md:justify-center">
+      <div className="mb-4 mt-8 inline-flex h-[54px] min-w-max shrink-0 snap-x snap-mandatory items-center justify-center gap-18 px-4 md:px-0">
         {/*Title */}
         <div className="hidden flex-col items-end justify-end gap-1 md:inline-flex">
           <div className="text-Gray-N700 w-full text-end text-[25px] leading-10 font-bold lg:mr-0">فلوی</div>
@@ -18,56 +18,113 @@ export default function Benefits() {
         </div>
 
         {/* Benefits */}
-        <div className="mx-5 flex items-center justify-start gap-8 md:mx-0">
+        <div className="flex items-center justify-start gap-3 sm:gap-5 md:gap-8 md:mx-5">
           {/* Benefit 1 */}
-          <div className="flex items-center justify-end gap-4">
-            <div className="relative size-[39.02px]">
-              <Image src={flashBg} alt="flash-bg" width={39} height={39} className="absolute top-0 left-0" />
+          <div className="flex items-center justify-end gap-3 sm:gap-3.5 md:gap-4">
+            <div className="relative size-7 sm:size-9 md:size-[39.02px]">
+              <div className="size-[21.85px] absolute top-[4.78px] left-[4.78px] bg-Primary-P100 rounded-[20.07px] sm:size-[26px] sm:top-[6px] sm:left-[6px] md:hidden"></div>
+              <Image 
+                src={flashBg} 
+                alt="flash-bg" 
+                width={39} 
+                height={39} 
+                className="absolute top-0 left-0 hidden md:block" 
+              />
+              <Image
+                src={flashOutline}
+                alt="flash-outline"
+                width={27}
+                height={27}
+                className="absolute top-0 left-0 size-[27.32px] sm:size-9 md:hidden"
+              />
               <Image
                 src={flashOutline}
                 alt="flash-outline"
                 width={35}
                 height={35}
-                className="absolute top-[-1.5px] left-[1px]"
+                className="absolute top-[-1.5px] left-[1px] hidden md:block"
               />
             </div>
-            <span className="text-Gray-N700 text-right text-base leading-7 font-medium">سریع‌ترین روش خرید</span>
+            <span className="text-Gray-N700 text-right text-[13px] sm:text-[15px] leading-normal md:text-base md:leading-7 font-medium">
+              <span className="md:hidden">سریع‌ترین روش خرید!</span>
+              <span className="hidden md:inline">سریع‌ترین روش خرید</span>
+            </span>
           </div>
 
           {/* Divider */}
-          <div className="mx-4 h-12 w-px bg-slate-200" />
+          <div className="w-px self-stretch bg-Gray-N200 sm:mx-2 md:mx-4 md:h-12 md:self-auto md:bg-slate-200" />
 
           {/* Benefit 2 */}
-          <div className="flex items-center justify-end gap-4">
-            <div className="relative size-[39.02px]">
-              <Image src={lampBg} alt="lamp-bg" width={39} height={39} className="absolute top-0 left-0" />
+          <div className="flex items-center justify-end gap-3 sm:gap-3.5 md:gap-4">
+            <div className="relative size-7 sm:size-9 md:size-[39.02px]">
+              <Image 
+                src={lampBg} 
+                alt="lamp-bg" 
+                width={27} 
+                height={27} 
+                className="absolute top-[1.37px] left-[2.73px] sm:size-9 sm:top-[2px] sm:left-[3px] md:hidden" 
+              />
+              <Image 
+                src={lampBg} 
+                alt="lamp-bg" 
+                width={39} 
+                height={39} 
+                className="absolute top-0 left-0 hidden md:block" 
+              />
+              <Image
+                src={lampOutline}
+                alt="lamp-outline"
+                width={27}
+                height={27}
+                className="absolute top-0 left-0 sm:size-9 md:hidden"
+              />
               <Image
                 src={lampOutline}
                 alt="lamp-outline"
                 width={34.96}
                 height={34.96}
-                className="absolute top-[1.5px] left-[-1px]"
+                className="absolute top-[1.5px] left-[-1px] hidden md:block"
               />
             </div>
-            <span className="text-Gray-N700 text-right text-base leading-7 font-medium">ارزان‌ترین قیمت</span>
+            <span className="text-Gray-N700 text-right text-[13px] sm:text-[15px] leading-normal md:text-base md:leading-7 font-medium">ارزان‌ترین قیمت</span>
           </div>
 
           {/* Divider */}
-          <div className="mx-4 h-12 w-px bg-slate-200" />
+          <div className="w-px self-stretch bg-Gray-N200 sm:mx-2 md:mx-4 md:h-12 md:self-auto md:bg-slate-200" />
 
           {/* Benefit 3 */}
-          <div className="flex items-center justify-end gap-4">
-            <div className="relative size-[39.02px]">
-              <Image src={crownBg} alt="crown-bg" width={39} height={39} className="absolute top-0 left-0" />
+          <div className="flex items-center justify-end gap-3 sm:gap-3.5 md:gap-4">
+            <div className="relative size-7 sm:size-9 md:size-[39.02px]">
+              <Image 
+                src={crownBg} 
+                alt="crown-bg" 
+                width={27} 
+                height={27} 
+                className="absolute top-[1.37px] left-[2.73px] sm:size-9 sm:top-[2px] sm:left-[3px] md:hidden" 
+              />
+              <Image 
+                src={crownBg} 
+                alt="crown-bg" 
+                width={39} 
+                height={39} 
+                className="absolute top-0 left-0 hidden md:block" 
+              />
+              <Image
+                src={crownOutline}
+                alt="crown-outline"
+                width={27}
+                height={27}
+                className="absolute top-0 left-0 sm:size-9 md:hidden"
+              />
               <Image
                 src={crownOutline}
                 alt="crown-outline"
                 width={34.96}
                 height={34.96}
-                className="absolute top-[2.5px] left-[-1px]"
+                className="absolute top-[2.5px] left-[-1px] hidden md:block"
               />
             </div>
-            <span className="text-Gray-N700 text-right text-base leading-7 font-medium">بهترین برای مقایسه</span>
+            <span className="text-Gray-N700 text-right text-[13px] sm:text-[15px] leading-normal md:text-base md:leading-7 font-medium">بهترین برای مقایسه</span>
           </div>
         </div>
       </div>
--- components/LandingPage/Companies.tsx
+++ components/LandingPage/Companies.tsx
@@ -4,25 +4,25 @@ export default function Companies() {
     <div className="my-4 md:my-6 w-full overflow-x-auto lg:flex lg:items-center lg:justify-center">
       <div className="flex w-full min-w-max shrink-0 snap-x snap-mandatory flex-col items-center justify-center gap-2 px-2 py-6">
         <div className="flex items-center justify-start gap-6 lg:justify-center">
-          <div className="mx-5 flex flex-row items-center justify-center gap-10 md:mx-0 md:gap-20 lg:gap-26 xl:gap-33">
+          <div className="mx-5 flex flex-row items-center justify-center gap-8 sm:gap-10 md:mx-0 md:gap-14 lg:gap-26 xl:gap-33">
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="h-9 object-contain" src="/images/alibaba.png" alt="Alibaba" width={100} height={36} />
+            <Image className="h-7 sm:h-9 object-contain" src="/images/alibaba.png" alt="Alibaba" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="h-9 object-contain" src="/images/MrBilit.png" alt="safarmarket" width={100} height={36} />
+            <Image className="h-7 sm:h-9 object-contain" src="/images/MrBilit.png" alt="safarmarket" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="h-9 object-contain" src="/images/flightio.png" alt="Flightio" width={100} height={36} />
+            <Image className="h-7 sm:h-9 object-contain" src="/images/flightio.png" alt="Flightio" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="h-9 object-contain" src="/images/flytoday.png" alt="Felanito" width={100} height={36} />
+            <Image className="h-7 sm:h-9 object-contain" src="/images/flytoday.png" alt="Felanito" width={100} height={36} />
             {/* </div> */}
 
             {/* <div className="flex flex-1 flex-col items-start justify-start gap-2 h-4"> */}
-            <Image className="h-9 object-contain" src="/images/snapptrip.png" alt="SnappTrip" width={100} height={36} />
+            <Image className="h-7 sm:h-9 object-contain" src="/images/snapptrip.png" alt="SnappTrip" width={100} height={36} />
             {/* </div> */}
           </div>
           {/*  todo : add other websites */}
