8bf1371d6f18f63736a038d9689c73e938b48c1d
Amir Hossein Hanifehzadeh <amirhanife84@gmail.com>
2025-11-13 18:50:09 +0330

    fix

--- components/Eitaa/EitaaDynamicAutoAuth/EitaaDynamicAutoAuth.tsx
+++ components/Eitaa/EitaaDynamicAutoAuth/EitaaDynamicAutoAuth.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic"
 
-const EitaaAutoAuth = dynamic(() => import("@/components/Eitaa/EitaaAutoAuth/EitaaAutoAuth"), { ssr: false })
+const EitaaAutoAuth = dynamic(() => import("@/components/Eitaa/EitaaAutoAuth/EitaaAutoAuth"), { ssr: true })
 
 export default function EitaaDynamicAutoAuth() {
   return <EitaaAutoAuth />
