77bc60e6b26c17b79218463788be401d6c307337
Mahya Cheraghi <mahya_ch@yahoo.com>
2025-11-25 23:53:24 +0330

    fix(style/test): omg

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test"
 
 test("has title", async ({ page }) => {
-  await page.goto("/")
+  await page.goto("/", { waitUntil: "domcontentloaded" })
 
   await expect(page).toHaveTitle(/فلوی | مقایسه بلیط هواپیما/)
 })
